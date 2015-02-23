30edc9c910f95c07632f5f93784101087d207ce9
Daan Kuitenbrouwer <daankuitenbrouwer@gmail.com>
2015-02-23 20:55:39 +0100

    working programme, however pressure not precisely what it should be.

--- forces4.f90
+++ forces4.f90
@@ -5,10 +5,10 @@
       integer, intent(in)  :: N
       real(8), intent(in)  :: pos(N,3),l
       real(8), intent(inout)  :: forces(N,3),pot, distances(N,N)
-      real(8), intent(inout) :: presvirial(1)
+      real(8), intent(inout) :: presvirial(1)        
 !f2py intent (in,out) :: forces, pot, distances, presvirial
       real(8), parameter :: rc =3.2
-      real(8) :: dr_vec(3),partialforce(3),dr2,F,partialpot
+      real(8) :: dr_vec(3),partialforce(3),dr2,F,partialpot,cnt
       integer :: i,j
       
           
@@ -16,6 +16,7 @@
       forces(:,:) = 0.0 
       pot = 0.0
       distances(:,:) = 0.0
+      cnt = 0.0
       
       !calculating the radial distance between each pair of particles
       !Afterwards, do forces
@@ -28,18 +29,19 @@
           distances(j,i) = sqrt(dr2)
           if (dr2 < rc*rc) then
             dr2 = 1/dr2
-            f= 2*dr2**7 - dr2**4
+            f = 2*dr2**7 - dr2**4
             partialforce(:) = 24*dr_vec*f
             forces(j,:) = forces(j,:) - partialforce(:)
             forces(i,:) = forces(i,:) + partialforce(:)
             presvirial = presvirial + dot_product(dr_vec,partialforce)
             partialpot = 4*(dr2**6 - dr2**3)
             pot = pot + partialpot
+            cnt = cnt + 1
           end if
         enddo
       enddo
-
- 
+      presvirial = presvirial/cnt
+       
       end subroutine calc_forces
       
     
--- mol_dy2.py
+++ mol_dy2.py
@@ -7,6 +7,7 @@ import math
 import matplotlib.pyplot as plt
 import pylab
 from mpl_toolkits.mplot3d import Axes3D
+import sys
 
 
 
@@ -20,15 +21,15 @@ corrflag = 2
 
 # independent parameters
 dt = 0.004
-N=864
+N=108
 lpnum = 1000
 cutoff = int(lpnum/5.)
-density = 0.85
+density = 0.88
 temp = 0.8
 Ttarg = 1
 Kb =  1
 nbins = 100 #number of radial shells
-n = 30 #number of timesteps per timeblock in pressure calculation
+n = 20 #number of timesteps per timeblock in pressure calculation
 
 
 # Loading initial conditions
@@ -41,16 +42,19 @@ toten = init_sys.init_toten(lpnum)
 prestime = init_sys.init_presvirialtime(lpnum-cutoff)
 kenarray = init_sys.init_kenarray(lpnum)
 
+
 # Iteration Verlet method
   # Initializing the first values for the loop
 pot = 0.0
-forces, pot, distances, presvirial = fm.calc_forces(pos,forces,pot,l,distances,[N])
+presvirial = 0.0
+forces, pot, distances, presvirial = fm.calc_forces(pos,forces,pot,l,distances,presvirial,[N])
 lamda=0
 cnt = 0
+
 for lp in range(lpnum):
   mom = mom + forces*0.5*dt
   pos = (pos + mom*dt) % l           # % l means modulo of l, hence it adds/subtracts n*l untill 0<pos<l
-  forces, pot, distances, presvirial = fm.calc_forces(pos,forces,pot,l,distances,[N])
+  forces, pot, distances, presvirial = fm.calc_forces(pos,forces,pot,l,distances, presvirial, [N])
   mom = mom + forces*0.5*dt
   Ken = np.sum(mom*mom*0.5)
   kenarray[lp,:] = Ken
@@ -65,15 +69,12 @@ for lp in range(lpnum):
     bin_vec_tot = bin_vec_tot + correlationmodule.cor(npdim,N,distances,nbins,finalbins,corrflag)
     corrflag = 1
   elif corrflag ==2:
-    prestime[cnt,:] = density*((2*Ken/(3*N)) - presvirial[0]/(3*N)) #Here we assume that the integral term to calculate the pressure is 0.
-    
+    prestime[cnt,:] = density*((2*Ken/(3*N)) + presvirial/(3*N) + np.pi*N*0.97596/Ken) 
     cnt = cnt + 1
-#print prestime    
-#print density*((2*Ken/(3*N)) - presvirial[0]/(3*N)), prestime[-1][0]
-print density*(2*Ken/(3*N))
+
+
 averagedp = np.zeros((lpnum-cutoff)/n, dtype=float)
 for p in range((lpnum-cutoff)/n):
-  #print presvirialtime[n*p:n*(p+1)], p
   averagedp[p] = np.mean(prestime[n*p:n*(p+1)])
 
 
@@ -85,9 +86,11 @@ for p in range((lpnum-cutoff)/n):
 fig4 = plt.plot(averagedp)
 plt.show()
 
+print min(prestime), max(prestime)
+
 mnp = np.mean(prestime)
 sdomp = np.std(prestime)/np.sqrt(len(prestime))
-mnavp = np.mean(averagedp)
+mnavp = sum(averagedp)/len(averagedp)
 sdomavp = np.std(averagedp)/np.sqrt(len(averagedp))
 
 print mnp, sdomp, mnavp, sdomavp
